0d5cce4019ef0b9e659d5d12b23608d9a6193cca
Rhowin Tony <mail2rhowintony.j@gmail.com>
2022-04-29 15:23:28 +0530

    final commit

--- main.py
+++ main.py
@@ -7,9 +7,23 @@ def fact(n):
     else:
         return n*fact(n-1)
 
-try:
-    input_val = int(input("Enter any number "))
-except ValueError as e:
-    print(f"Input is not valid, Please re-run with valid input. - {e}")
-else:
-    
+def int_check(n):
+        check_val = int(n)
+        if check_val < 0:
+            raise ValueError("Value is less than 0.")
+        else:
+            return check_val
+
+def main():
+    try:
+        input_val = (input("Enter any number "))
+        checked_int = int_check(input_val)
+    except ValueError as e:
+        print(f"Input is not valid, Please re-run with valid input. - {e}")
+    else:
+        print(f"The factorial is {fact(checked_int)}")
+
+main()
+        
+
+
