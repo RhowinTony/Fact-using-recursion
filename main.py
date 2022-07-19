8515b2410eec81e74bd87ee9ee2ff0687f3e751a
Rhowin Tony <mail2rhowintony.j@gmail.com>
2022-07-19 23:55:29 +0530

    next commmit

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 #Write a program to calculate factorial of a number using recursion
 
-# def fact(n):
-#     if n == 0:
-#         return 1
-#     else:
-#         return n*fact(n-1)
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n*fact(n-1)
 
-# def int_check(n):
-#         check_val = int(n)
-#         if check_val < 0:
-#             raise ValueError("Value is less than 0.")
-#         else:
-#             return check_val
+def int_check(n):
+        check_val = int(n)
+        if check_val < 0:
+            raise ValueError("Value is less than 0.")
+        else:
+            return check_val
 
-# def main():
-#     try:
-#         input_val = (input("Enter any number "))
-#         checked_int = int_check(input_val)
-#     except ValueError as e:
-#         print(f"Input is not valid, Please re-run with valid input. - {e}")
-#     else:
-#         print(f"The factorial is {fact(checked_int)}")
+def main():
+    try:
+        input_val = (input("Enter any number "))
+        checked_int = int_check(input_val)
+    except ValueError as e:
+        print(f"Input is not valid, Please re-run with valid input. - {e}")
+    else:
+        print(f"The factorial is {fact(checked_int)}")
 
-# main()
+main()
